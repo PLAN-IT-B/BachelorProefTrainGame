6157b9e52b715a0f2858837c357fa0b8a8bcefb6
MicVD01 <vandaelemichiel05@gmail.com>
2022-02-22 20:02:08 +0100

    Verfijning Drukknop.h

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,25 @@ int knop;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  
+  /*
   pinMode(12,INPUT);
   pinMode(13,INPUT);
   pinMode(14,INPUT);
+  */
+  int zone;
+  Drukknop *d1;
+  zone = d1->bepaalZone(1000,3000);
+  Serial.println(zone);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  /*
   x = analogRead(12);
   y = analogRead(14);
   knop = analogRead(13);
-  //cout<<x<<" "<<y<<" "<<knop<<endl;
   Serial.print(x);
   Serial.print("  ");
   Serial.print(y);
@@ -29,4 +37,6 @@ void loop() {
   Serial.print(knop);
   Serial.println();
   delay(1000);
+  */
+
 }
